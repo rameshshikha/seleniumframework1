66ff4fad9aff1beb3172017f40120ff402620099
Arushi <Arushi@ARUSHI>
2023-12-13 12:14:00 +0530

    committed first push

---
+++ MavenExecution/src/test/java/mavenTest/createnewTest.java
@@ -0,0 +1,13 @@
+package mavenTest;
+
+import org.testng.annotations.Test;
+
+public class createnewTest {
+
+	
+	@Test
+	public void myTest() {
+		System.out.println("testing successfull");
+	}
+
+}
